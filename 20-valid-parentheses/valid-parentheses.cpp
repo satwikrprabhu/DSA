f4e2b049a5c39e48e209fcd248e031a607ae47de
Satwik Prabhu <satwikprabhu@gmail.com>
2024-04-28 20:33:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (6.65%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,29 @@
+#include<stack>
+class Solution {
+public:
+    bool isValid(string str) {
+      stack <int> s;
+      for(int i=0;i<str.length();i++){
+        if(s.empty()){
+            s.push(str[i]);
+        }
+        else if(s.top()=='(' && str[i]==')'){
+             s.pop();
+        }
+        else if(s.top()=='[' && str[i]==']'){
+             s.pop();
+        }
+        else if(s.top()=='{' && str[i]=='}'){
+             s.pop();
+        }
+        else{
+             s.push(str[i]);
+        }
+      }
+      if(s.empty()){
+        return true;
+      } 
+      else
+        return false;
+    }
+};
