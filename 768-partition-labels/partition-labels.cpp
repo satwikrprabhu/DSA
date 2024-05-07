0a24386089f6d4c33e67d2154cd9b0ccaacf5964
Satwik Prabhu <satwikprabhu@gmail.com>
2024-05-08 00:01:42 +0530

    Time: 3 ms (73.27%) | Memory: 9.1 MB (56.17%) - LeetSync

--- 768-partition-labels/partition-labels.cpp
+++ 768-partition-labels/partition-labels.cpp
@@ -1,39 +1,21 @@
+#include<algorithm>
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
+        unordered_map<char,int> m;
         vector<int> a;
-        string str1="";
-        str1=s[0];
-      for(int i=1;i<s.length();i++){
-        bool flag1=false,flag2=false;
-        for(int j=0;j<str1.length();j++){
-           if(s[i]==str1[j]){
-            flag1=true;
-            break;
+        for(int i=0;i<s.length();i++){
+            m[s[i]]=i;
+        }
+        int maxNum=0,prev=-1;
+        for(int i=0;i<s.size();i++){
+           maxNum=max(maxNum,m[s[i]]);
+           if(i==maxNum){
+            a.push_back(maxNum-prev);
+            prev=maxNum;
            }
         }
-        for(int l=0;l<str1.length();l++){
-            for(int k=i+1;k<s.length();k++){
-                if(str1[l]==s[k]){
-                    flag2=true;
-                    break;
-                }
-            }
-        }
-
-
-        if(flag1==true || flag2==true){
-            str1=str1+s[i];
-            cout<<str1<<endl;
-        }
-        else{
-            cout<<i<<endl;
-            a.push_back(str1.length());
-            str1="";
-            str1=s[i];
-        }
-      }
-      a.push_back(str1.length());
-      return a;
+        return a;
     }
 };
+
