16595f3cf1cf03fe31ab54a2f2060f2a35f8cbb3
Satwik Prabhu <satwikprabhu@gmail.com>
2024-05-20 00:57:35 +0530

    Time: 50 ms (42.84%) | Memory: 30.3 MB (38.43%) - LeetSync

--- 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.cpp
+++ 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.cpp
@@ -3,18 +3,12 @@ public:
     vector<vector<int>> sortTheStudents(vector<vector<int>>& score, int k) {
         vector<vector<int>> arr;
         map<int,int> m;
-        vector<int> a;
         int row=score.size();
-        int col=score[0].size();
         for(int i=0;i<row;i++){
             m[score[i][k]]=i;
         }
         for(auto i=m.rbegin();i!=m.rend();i++){
-            vector <int> a;
-            for(int j=0;j<col;j++){
-                a.push_back(score[i->second][j]);
-            }
-            arr.push_back(a);
+            arr.push_back(score[i->second]);
         }
         return arr;
     }
