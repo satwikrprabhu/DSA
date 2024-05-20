dd2fc2b49ad6097321d550228760305f4bf920a6
Satwik Prabhu <satwikprabhu@gmail.com>
2024-05-20 15:49:18 +0530

    Time: 1 ms (54.22%) | Memory: 10.3 MB (18.11%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.cpp
@@ -0,0 +1,23 @@
+#include<vector>
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> res;
+        for(int i=1;i<=n;i++){
+            if(i%3==0 && i%5==0){
+                res.push_back("FizzBuzz");
+            }
+            else if(i%3==0){
+                 res.push_back("Fizz");
+            }
+            else if(i%5==0){
+                res.push_back("Buzz");
+            }
+            else{
+                string a=to_string(i);
+                res.push_back(a);
+            }
+        }
+        return res;
+    }
+};
