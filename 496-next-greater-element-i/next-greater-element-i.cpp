3028f0187229bd2eb775b5a6746937b75be61af7
Satwik Prabhu <satwikprabhu@gmail.com>
2024-06-08 16:15:48 +0530

    Time: 2 ms (86.85%) | Memory: 12.2 MB (11.78%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,27 +1,36 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector <int> a;
-        for(auto i:nums1){
-            for(int j=0;j<nums2.size();j++){
-                if(i==nums2[j]){
-                    while(i>=nums2[j]){
-                        if(j==nums2.size()-1){
-                            break;
-                        }
-                        j++;
-                    }
-                    if(i<nums2[j]){
-                        a.push_back(nums2[j]);
-                        break;
-                    }
-                    else{
-                        a.push_back(-1);
-                        break;
-                    }
-                }
+        stack<int> s;
+        unordered_map <int,int> m;
+        vector<int> res;
+        int n = nums2.size();
+        for(int i=n-1;i>=0;i--){
+            if(s.empty()){
+                m[nums2[i]] = -1;
+                s.push(nums2[i]);
+            }
+            else if(s.top()<nums2[i]){
+            while(!s.empty() && nums2[i]>s.top()){
+                s.pop();
+            }
+            if(s.empty()){
+                m[nums2[i]]=-1;
+                s.push(nums2[i]);
+            }
+            else{
+                m[nums2[i]]=s.top();
+                s.push(nums2[i]);
+            }
+            }
+            else{
+                m[nums2[i]]=s.top();
+                s.push(nums2[i]);
             }
         }
-        return a;
+        for(auto i:nums1){
+            res.push_back(m[i]);
+        }
+        return res;
     }
 };
