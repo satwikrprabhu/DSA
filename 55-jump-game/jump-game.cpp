6a85abe5a71007972c1ef0db31688f5327c4e266
Satwik Prabhu <satwikprabhu@gmail.com>
2024-06-24 17:57:12 +0530

    Time: 37 ms (87.40%) | Memory: 50.8 MB (81.74%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -4,7 +4,6 @@ public:
         int n=nums.size(),maxi=nums[0];
         for(int i=0;i<=maxi;i++){
             if(i+nums[i]>=n-1 && i<=maxi){
-                cout<<i+nums[i]<<" "<<n-1;
                 return true;
             }
         maxi = max(maxi,nums[i]+i);
